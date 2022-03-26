580239439a6745b472c6d170deb1c57fb41f1115
mahdi-frms <mahdif1380@outlook.com>
2022-03-26 20:16:43 +0430

    add basic command-line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::process::exit;
+
+fn print_help() {
+    println!("Usage: arsync [src] [dest]");
+}
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 3 {
+        print_help();
+        exit(1);
+    }
+    println!("copying from {} to {}", args[1], args[2]);
 }
