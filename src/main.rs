e3c62acdac0384f321782be43936c5aa9ece7c82
mahdi-frms <mahdif1380@outlook.com>
2022-03-27 10:58:31 +0430

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,22 @@ fn print_help() {
 fn traverse_dir(dir: &String) -> Result<Vec<Filerec>, std::io::Error> {
     let mut files = vec![];
     for entry in read_dir(dir)?.filter_map(|e| e.ok()) {
-        if let Ok(path) = entry.path().into_os_string().into_string() {
-            if let Ok(kind) = entry.file_type() {
-                if kind.is_dir() {
-                    files.append(&mut traverse_dir(&path).unwrap_or(vec![]));
-                } else if kind.is_file() {
-                    if let Ok(md) = entry.metadata() {
-                        if let Ok(time) = md.modified() {
-                            if let Ok(dur) = time.duration_since(SystemTime::UNIX_EPOCH) {
-                                files.push(Filerec {
-                                    path,
-                                    time: dur.as_millis(),
-                                });
-                            }
-                        }
-                    }
-                }
+        (|| {
+            let path = entry.path().into_os_string().into_string().ok()?;
+            let kind = entry.file_type().ok()?;
+            if kind.is_dir() {
+                files.append(&mut traverse_dir(&path).unwrap_or(vec![]));
+            } else if kind.is_file() {
+                let md = entry.metadata().ok()?;
+                let time = md.modified().ok()?;
+                let dur = time.duration_since(SystemTime::UNIX_EPOCH).ok()?;
+                files.push(Filerec {
+                    path,
+                    time: dur.as_millis(),
+                });
             }
-        }
+            Some(())
+        })();
     }
     Ok(files)
 }
